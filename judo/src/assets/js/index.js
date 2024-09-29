a1c4cfcd8d49a431e75f7b36ee5e036b7d1892d2
IvanSlivenko <ivan8822@ukr.net>
2024-09-29 21:06:51 +0300

    29-09-2024-21-06

--- judo/src/assets/js/index.js
+++ judo/src/assets/js/index.js
@@ -2,4 +2,27 @@ import '../styles/reset.scss';
 import '../styles/mixins.scss';
 import '../styles/styles.scss';
 
+const classes = {
+    opened: 'opened'
+}
+
+const  header = document.querySelector('.header');
+const menuLink = document.querySelectorAll('.menu-link');
+const menuButton =  document.querySelector('.header-menu__button'); 
+
+
+const toggleMenu = () => header.classList.toggle(classes.opened);
+
+const scrollTosection = (e) => {
+    e.preventDefault();
+    const href =   e.currentTarget.getAttribute('href');
+    console.log(href);
     
+
+
+};
+
+
+menuButton.addEventListener('click', toggleMenu);
+menuLink.forEach((link) => link.addEventListener('click', scrollTosection));
+
