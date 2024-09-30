49308815c4160e3113ac64fe40d0854ee23953f5
IvanSlivenko <ivan8822@ukr.net>
2024-09-30 10:41:08 +0300

    30-09-2024-10-41

--- judo/src/assets/js/index.js
+++ judo/src/assets/js/index.js
@@ -16,13 +16,45 @@ const toggleMenu = () => header.classList.toggle(classes.opened);
 const scrollTosection = (e) => {
     e.preventDefault();
     const href =   e.currentTarget.getAttribute('href');
-    console.log(href);
     
+    if(!href && !href.startsWith('#')) return;
 
+    const section = href.slice(1);
+    const top = document.getElementById(section)?.offsetTop || 0;
 
+    window.scrollTo({ top: top, behavior: "smooth"});
+    
 };
 
+const formatValue = () =>{};
 
+const getTimerValues = (diff) => {
+    return {
+        seconds: (diff / 1000) % 60,
+        minutes: (diff / (1000*60)) % 60,
+        hours: (diff / (1000*60*60)) % 24,
+        days: (diff / (1000*60*60*24)) % 30
+    }
+}
+
+const startTimer = (date) => {
+    setInterval(() => {
+        const diff = new Date(date).getTime() - new Date().getTime();
+        const values = getTimerValues(diff);
+        Object.entries(values).forEach(([key, value]) =>{ 
+            const timerValue = document.getElementById(key);
+            timerValue.innerText = value;
+            
+        });
+    }, 1000)
+    
+    console.log(getTimerValues(diff));
+    
+
+    
+}
+
+startTimer("October 29 2024 19:00:00");
 menuButton.addEventListener('click', toggleMenu);
 menuLink.forEach((link) => link.addEventListener('click', scrollTosection));
 
